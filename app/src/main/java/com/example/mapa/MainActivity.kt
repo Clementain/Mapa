1f6221c3c549122e5b07f4db8d547d22be7a9833
Clementain <clentecarmona@gmail.com>
2023-04-01 19:24:22 -0600

    Mapa ajustado al proyecto de clase

--- app/src/main/java/com/example/mapa/MainActivity.kt
+++ app/src/main/java/com/example/mapa/MainActivity.kt
@@ -2,21 +2,34 @@
 
 package com.example.mapa
 
-import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.OverlayItem
+import org.osmdroid.views.overlay.Polyline
 
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
+    private var firstMarker: Marker? = null
+    private lateinit var marker: Marker
     var map: MapView? = null
-    public override fun onCreate(savedInstanceState: Bundle?) {
+
+    //your items
+    var items = ArrayList<OverlayItem>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //handle permissions first, before map is created. not depicted here
+
+        //load/initialize the osmdroid configuration, this can be done
         //handle permissions first, before map is created. not depicted here
 
         //load/initialize the osmdroid configuration, this can be done
@@ -29,12 +42,52 @@ class MainActivity : Activity() {
         //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's tile servers will get you banned based on this string
 
         //inflate and create the map
+        //setting this before the layout is inflated is a good idea
+        //it 'should' ensure that the map has a writable location for the map cache, even without permissions
+        //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
+        //see also StorageUtils
+        //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's tile servers will get you banned based on this string
+
+        //inflate and create the map
         setContentView(R.layout.activity_main)
+
         map = findViewById<View>(R.id.map) as MapView
         map!!.setTileSource(TileSourceFactory.MAPNIK)
+
+        val mapController = map!!.controller
+        mapController.setZoom(19)
+        val startPoint = GeoPoint(20.140153689100682, -101.15067778465794)
+        mapController.setCenter(startPoint)
+
+        items.add(
+            OverlayItem(
+                "Title", "Description", GeoPoint(0.0, 0.0)
+            )
+        ) // Lat/Lon decimal degrees
+
+        firstMarker = Marker(map)
+        firstMarker?.position = startPoint
+        firstMarker?.setAnchor(Marker.ANCHOR_BOTTOM, Marker.ANCHOR_CENTER)
+        firstMarker?.title = "Bello ITSUR"
+        map?.overlays?.add(firstMarker)
+
+        map?.invalidate()
+
+        //add your points here
+        val line = Polyline()   //see note below!
+
+        line.setPoints(
+            arrayListOf(
+                startPoint,
+                GeoPoint(20.140462055482093, -101.15053861935188),
+                GeoPoint(20.14341707158446, -101.14984874847927),
+                GeoPoint(20.14395683454409, -101.15131101775268)
+            )
+        )
+        map?.overlays?.add(line)
     }
 
-    public override fun onResume() {
+    override fun onResume() {
         super.onResume()
         //this will refresh the osmdroid configuration on resuming.
         //if you make changes to the configuration, use
@@ -43,7 +96,7 @@ class MainActivity : Activity() {
         map!!.onResume() //needed for compass, my location overlays, v6.0.0 and up
     }
 
-    public override fun onPause() {
+    override fun onPause() {
         super.onPause()
         //this will refresh the osmdroid configuration on resuming.
         //if you make changes to the configuration, use
@@ -51,4 +104,5 @@ class MainActivity : Activity() {
         //Configuration.getInstance().save(this, prefs);
         map!!.onPause() //needed for compass, my location overlays, v6.0.0 and up
     }
+
 }
