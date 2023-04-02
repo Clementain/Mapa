909560ab8cd9c675ad7a87d5d9c5797e3accc35e
Clementain <clentecarmona@gmail.com>
2023-04-01 19:59:14 -0600

    se corrigió la solicitud de permisos

--- app/src/main/java/com/example/mapa/MainActivity.kt
+++ app/src/main/java/com/example/mapa/MainActivity.kt
@@ -125,14 +125,9 @@ class MainActivity : AppCompatActivity() {
                 this, Manifest.permission.ACCESS_COARSE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED
         ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_PERMISSION
+            )
         }
         locationManager.requestLocationUpdates(
             LocationManager.GPS_PROVIDER, 5000, 10f, locationListener
