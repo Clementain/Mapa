750b7c00c338666f367b556ebb580529af28fdff
Clementain <clentecarmona@gmail.com>
2023-04-03 19:02:19 -0500

    Los permisos ya se solicitan en ejecucion, persiste el error de ubicacion, si hay suerte, en el siguiente commit se arregla, y si no, aparecerea ya otro en otra fecha, ya no hoy

--- app/src/main/java/com/example/mapa/MainActivity.kt
+++ app/src/main/java/com/example/mapa/MainActivity.kt
@@ -2,11 +2,15 @@
 
 package com.example.mapa
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.mapa.API.Direcciones
 import com.example.mapa.API.DireccionesApi
 import kotlinx.coroutines.CoroutineScope
@@ -35,7 +39,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        checkPermissions()
         val ctx: Context = applicationContext
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx))
 
@@ -102,5 +106,40 @@ class MainActivity : AppCompatActivity() {
 
         map!!.onPause()
     }
+
+    fun checkPermissions() {
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
+                this, Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ), 0
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            0 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    return
+                } else {
+                    checkPermissions()
+                }
+            }
+        }
+    }
+
 
 }
